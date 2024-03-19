cd9379ce29d139d53b52bf9be3fb41718c920ec7
victor <victorhugomotabueno@gmail.com>
2024-03-19 09:15:26 -0300

    Login e Recuperar Senha

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,38 +1,69 @@
 import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
-
+ 
 // Importando os recursos de autenticação
 import { auth } from "../../firebase.config";
-
-// Importando a função de login com e-mail e senha
-import { signInWithEmailAndPassword } from "firebase/auth";
-
+ 
+// Importando a função de login com o e-mail e senha
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+ 
 import { useState } from "react";
-
-export default function Login() {
+ 
+export default function Login( { navigation } ) {
+ 
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-
+ 
   const login = async () => {
-    if (!email || !senha) {
-      Alert.alert("Atencão!", "Preencha e-mail e senha!");
-      return;
-    }
-    console.log(email, senha);
+  if(!email || !senha){
+    Alert.alert("Atenção", "Preenha e-mail e senha!");
+    return;
   }
-
+ 
+  try {
+    await signInWithEmailAndPassword(auth, email, senha);
+    navigation.replace("AreaLogada")
+  } catch (error) {
+    console.error(error.code);
+    let mensagem;
+    switch(error.code){
+      case "auth/invalid-credential":
+        mensagem = "Dados inválidos!";
+        break;
+      case "auth/invalid-email":
+        mensagem = "Endereço e de e-mail inválido!";
+        break;
+      default:
+        mensagem = "Houve um erro, tente novamente mais tarde!";
+        break;
+    }
+    Alert.alert("Ops!", mensagem);
+  }
+ 
+  };
+ 
+  const recuperarSenha = async () => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      Alert.alert("Recuperar senha", "Verifique sua caixa de e-mails.");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+ 
   return (
     <View style={estilos.container}>
       <View style={estilos.formulario}>
         <TextInput onChangeText={(valor) => setEmail(valor)} placeholder="E-mail" style={estilos.input} />
-        <TextInput  onChangeText={(valor) => setSenha(valor)} placeholder="Senha" secureTextEntry style={estilos.input} />
+        <TextInput onChangeText={(valor) => setSenha(valor)} placeholder="Senha" style={estilos.input} secureTextEntry />
         <View style={estilos.botoes}>
           <Button onPress={login} title="Entre" color="green" />
+          <Button title="Recuperar senha" color="grey" onPress={recuperarSenha} />
         </View>
       </View>
     </View>
   );
 }
-
+ 
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,3 +88,4 @@ const estilos = StyleSheet.create({
     justifyContent: "space-between",
   },
 });
+ 
