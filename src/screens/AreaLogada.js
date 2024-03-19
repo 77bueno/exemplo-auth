ca6603e5a3813845bb7e33b47290de3210627359
victor <victorhugomotabueno@gmail.com>
2024-03-19 09:24:41 -0300

    Recuperando dados de usuario logado

--- src/screens/AreaLogada.js
+++ src/screens/AreaLogada.js
@@ -1,10 +1,18 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { auth } from "../../firebase.config";
 
 export default function AreaLogada() {
+  console.log(auth.currentUser);
+
+  const { email } = auth.currentUser;
+  console.log(email);
+
   return (
     <View style={estilos.container}>
       <View style={estilos.topo}>
         <Text style={estilos.bemVindo}>Bem-vindo(a)</Text>
+        <Text>Seu e-mail de cadastro é: </Text>
+        <Text style={{ fontWeight: "bold" }}>{email}</Text>
         <Button title="Logout" color="#D35400" />
       </View>
       <View style={estilos.geral}>
